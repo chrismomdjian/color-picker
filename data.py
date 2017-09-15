3f1b8c5b3e9d85e785c9b582cfb032a56a03e510
chrismomdjian <chrismomdjian@gmail.com>
2017-09-14 18:51:59 -0700

    modified code to be shorter and simpler

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 colors = [
-    {"color": "red", "hex": "#e74c3c", "rgba": [231, 76, 60, 1]},
-    {"color": "blue", "hex": "#2980b9", "rgba": [41, 128, 185, 1]},
-    {"color": "green", "hex": "#27ae60", "rgba": [39, 174, 96, 1]},
-    {"color": "orange", "hex": "#f39c12", "rgba": [243, 156, 18, 1]},
-    {"color": "purple", "hex": "#9b59b6", "rgba": [155, 89, 182, 1]},
-    {"color": "yellow", "hex": "#f1c40f", "rgba": [241, 196, 15, 1]},
-    {"color": "brown", "hex": "#471c10", "rgba": [71, 28, 16, 1]},
-    {"color": "pink", "hex": "#ba6148", "rgba": [186, 97, 72, 1]},
-    {"color": "black", "hex": "#000000", "rgba": [0, 0, 0, 1]}
+    {"color": "red", "hex": "#e74c3c", "rgba": "231, 76, 60, 1"},
+    {"color": "blue", "hex": "#2980b9", "rgba": "41, 128, 185, 1"},
+    {"color": "green", "hex": "#27ae60", "rgba": "39, 174, 96, 1"},
+    {"color": "orange", "hex": "#f39c12", "rgba": "243, 156, 18, 1"},
+    {"color": "purple", "hex": "#9b59b6", "rgba": "155, 89, 182, 1"},
+    {"color": "yellow", "hex": "#f1c40f", "rgba": "241, 196, 15, 1"},
+    {"color": "brown", "hex": "#471c10", "rgba": "71, 28, 16, 1"},
+    {"color": "pink", "hex": "#ba6148", "rgba": "186, 97, 72, 1"},
+    {"color": "black", "hex": "#000000", "rgba": "0, 0, 0, 1"}
 ]
