88c53bb3fd32ea1dc4fcd54f592e3a22415170c7
chrismomdjian <chrismomdjian@gmail.com>
2017-09-14 17:51:00 -0700

    added logic for hex and rgba

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 colors = [
-    {"color": "red"},
-    {"color": "blue"},
-    {"color": "green"},
-    {"color": "orange"},
-    {"color": "purple"},
-    {"color": "yellow"},
-    {"color": "brown"},
-    {"color": "pink"},
-    {"color": "black"}
+    {"color": "red", "hex": "#e74c3c", "rgba": [231, 76, 60, 1]},
+    {"color": "blue", "hex": "#2980b9", "rgba": [231, 76, 60, 1]},
+    {"color": "green", "hex": "#27ae60", "rgba": [231, 76, 60, 1]},
+    {"color": "orange", "hex": "#f39c12", "rgba": [231, 76, 60, 1]},
+    {"color": "purple", "hex": "#9b59b6", "rgba": [231, 76, 60, 1]},
+    {"color": "yellow", "hex": "#f1c40f", "rgba": [231, 76, 60, 1]},
+    {"color": "brown", "hex": "#471c10", "rgba": [231, 76, 60, 1]},
+    {"color": "pink", "hex": "#ba6148", "rgba": [231, 76, 60, 1]},
+    {"color": "black", "hex": "#000000", "rgba": [231, 76, 60, 1]}
 ]
