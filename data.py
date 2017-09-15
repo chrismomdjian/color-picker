1767511a550db91a71b4cfb94d9577b3fd581946
chrismomdjian <chrismomdjian@gmail.com>
2017-09-14 17:06:52 -0700

    first commit

---
+++ data.py
@@ -0,0 +1,11 @@
+colors = [
+    {"color": "red"},
+    {"color": "blue"},
+    {"color": "green"},
+    {"color": "orange"},
+    {"color": "purple"},
+    {"color": "yellow"},
+    {"color": "brown"},
+    {"color": "pink"},
+    {"color": "black"}
+]
---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+from data import colors
+
+app = Flask(__name__)
+
+@app.route("/home/<string:color>", methods=["GET"])
+def home(color):
+    choices = [choice for choice in colors]
+    return render_template("home.html", color=color, choices=choices)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
